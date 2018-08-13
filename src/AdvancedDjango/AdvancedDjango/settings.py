a6b38f851d405611029414d8b2b588952339a7c1
LittleElijah <cuiyeqing960904@gmail.com>
2018-08-13 16:17:08 +0300

    refactored

--- src/AdvancedDjango/AdvancedDjango/settings.py
+++ src/AdvancedDjango/AdvancedDjango/settings.py
@@ -31,8 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'products',
-    'pages',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
