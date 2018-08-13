4c29bd2068dcafd2524605f025401ec70932fe6f
LittleElijah <cuiyeqing960904@gmail.com>
2018-08-13 17:51:58 +0300

    login redirect

--- src/AdvancedDjango/AdvancedDjango/urls.py
+++ src/AdvancedDjango/AdvancedDjango/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url, include
 from django.contrib import admin
+from .views import login_redirect
 
 urlpatterns = [
+    url(r'^$', login_redirect, name="login_redirect"),
     url(r'^admin/', admin.site.urls),
     url(r'^account/', include('accounts.urls'))
 ]
