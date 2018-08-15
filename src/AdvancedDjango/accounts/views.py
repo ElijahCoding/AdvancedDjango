506328ff1e405e513b2f6c6568df188ea63f38a4
LittleElijah <cuiyeqing960904@gmail.com>
2018-08-15 16:03:47 +0300

    change user profile using customized UserChangeForm

--- src/AdvancedDjango/accounts/views.py
+++ src/AdvancedDjango/accounts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, HttpResponse, redirect
 from django.contrib.auth.forms import UserCreationForm
-from .forms import RegistrationForm
+from .forms import RegistrationForm, EditProfileForm
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserChangeForm
 
@@ -28,12 +28,12 @@ def view_profile(request):
 
 def edit_profile(request):
     if request.method == 'POST':
-        form = UserChangeForm(request.POST, instance=request.user)
+        form = EditProfileForm(request.POST, instance=request.user)
 
         if form.is_valid():
             form.save()
             return redirect('/account/profile')
     else:
-        form = UserChangeForm(instance=request.user)
+        form = EditProfileForm(instance=request.user)
         args = { 'form': form }
         return render(request, 'accounts/edit_profile.html', args)
