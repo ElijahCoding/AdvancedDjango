6f7add16418b95ba69684eb262256c7c7aa0c232
LittleElijah <cuiyeqing960904@gmail.com>
2018-08-19 19:16:41 +0300

    finish resetting password

--- src/AdvancedDjango/accounts/urls.py
+++ src/AdvancedDjango/accounts/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 from . import views
-from django.contrib.auth.views import login, logout, password_reset, password_reset_done, password_reset_confirm
+from django.contrib.auth.views import login, logout, password_reset, password_reset_done, password_reset_confirm, password_reset_complete
 
 urlpatterns = [
     url(r'^$', views.home),
@@ -13,4 +13,5 @@ urlpatterns = [
     url(r'^reset-password/$', password_reset, name="password_reset"),
     url(r'^reset-password/done/$', password_reset_done, name="password_reset_done"),
     url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', password_reset_confirm, name='password_reset_confirm'),
+    url(r'^reset-password/complete/$', password_reset_complete, name="password_reset_complete")
 ]
